12bfcd6365b12f84b15bdf946b9ad003a7b38fbc
Danny Luong <dannyluong668@gmail.com>
2019-09-21 12:47:43 -0400

    fixed

--- Luong_p1/src/Decrypter.java
+++ Luong_p1/src/Decrypter.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Decrypter {
 
 	public static void main(String[] args) {
--- Luong_p1/src/Encrypter.java
+++ Luong_p1/src/Encrypter.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+
 public class Encrypter {
 
 	public static void main(String[] args) {
